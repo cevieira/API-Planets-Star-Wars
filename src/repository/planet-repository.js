a8fb84d11dab06f95cce5e94e748abc90366282b
Carlos Vieira <ce.vieira98@gmail.com>
2020-01-05 10:38:11 -0300

    Corrigindo bug da busca

--- src/repository/planet-repository.js
+++ src/repository/planet-repository.js
@@ -4,17 +4,24 @@ const mongoose = require('mongoose');
 const Planet = mongoose.model('Planet');
 
 exports.get = async (req) => {
-
     var pageOptions = {
-        page: parseInt(req.query.page) || 0,
+        page: parseInt(req.query.page) || 1,
         limit: parseInt(req.query.limit) || 10,
         order: 'asc',
-        orderBy: 'name'
-    };
-
-    const res = await Planet.paginate({
-        orderBy: pageOptions.orderBy,
-        sort: { [orderBy]: pageOptions.order },
+        orderBy: 'name',
+        searchBy: 'name',
+        search: ''
+    }
+    const res = await Planet.paginate(
+        {
+            [pageOptions.searchBy]:
+            {
+                $regex: `${pageOptions.search}`
+            }
+        }, {
+        sort: {
+            [pageOptions.orderBy]: pageOptions.order
+        },
         limit: pageOptions.limit,
         page: pageOptions.page
     });
