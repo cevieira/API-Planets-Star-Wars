420b883d656961ed24d8681a99ab33a07889f5c6
Carlos Vieira <ce.vieira98@gmail.com>
2020-01-05 10:43:39 -0300

    Adicionando validação para evitar que o número de planetas utilizado seja referente a um planeta com outro nome

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -6,8 +6,15 @@ const swapi = (planet) => {
     let planetUrl = `https://swapi.co/api/planets/?search=${planet.name}`;
     return axios.get(planetUrl).then(response => {
         if (response.data.count > 0) {
-            planet.numberOfMovies = response.data.results[0].films.length;
-            return planet;
+            //Fazendo essa verificação pois a busca feita no Swapi utiliza like, 
+            //podendo trazer outros planetas com o nome diferente, comprometendo o resultado.
+            if (response.data.results[0].name == planet.name) {
+                planet.numberOfMovies = response.data.results[0].films.length;
+                return planet;
+            } else {
+                return planet;
+            }
+
         }
         else {
             return planet;
