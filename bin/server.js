2f7e0eb90892c0dff2b03838902b073e16818a67
Carlos Vieira <ce.vieira98@gmail.com>
2020-01-04 22:05:30 -0300

    Alterando package.json para corrigir o script de inicialização, incluindo o config.js para poder publicar no Heroku, alterando server.js para encontrar corretamente a porta ao publicar

--- bin/server.js
+++ bin/server.js
@@ -3,7 +3,7 @@
 const http = require('http');
 const app = require('../src/app')
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.set('port', port);
 
 const server = http.createServer(app);
---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    connectionString: 'mongodb://cevieira:cevieira98@ds141218.mlab.com:41218/node_planets'
+}
